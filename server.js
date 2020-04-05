6885f9f44d6583fc60f324c60dd4a2d76228071d
Beau Blyth <teknopants@gmail.com>
2020-04-05 13:13:19 -0700

    response . render in server

--- server.js
+++ server.js
@@ -3,71 +3,72 @@
  * @author Beau Blyth
 */
 
-const PORT = process.env.PORT || 5000
-
 // Dependencies
-const express = require('express');
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketIO = require('socket.io')
 
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
+const PORT = process.env.PORT || 5000
 
 
 // Initialization
-const app = express();
-const server = http.Server(app);
-const io = socketIO(server);
+const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
-app.set('port', PORT);
-app.use('/static', express.static(path.join(__dirname + '/static')));
+app.set('port', PORT)
+app.use('/static', express.static(path.join(__dirname + '/static')))
 
 // Routing
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, 'views/index.html'))
+    //response.sendFile(path.join(__dirname, 'static/index.html'))
+    response.render('static/index')
 })
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 // Starts the server.
 server.listen(5000, function () {
-    console.log('Starting server on port 5000');
-});
+    console.log('Starting server on port 5000')
+})
 
 
 // Add the WebSocket handlers
-var players = {};
+var players = {}
 io.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         // remove disconnected player
-    });
+    })
 
     socket.on('new player', function () {
         players[socket.id] = {
             x: 300,
             y: 300
-        };
-    });
+        }
+    })
 
     socket.on('playerPosition', function (position, speed, timeStamp) {
-        var _currentTime = (new Date()).getTime();
-        var _timeElapsed = (_currentTime - timeStamp) / 1000;
-        var player = players[socket.id] || {};
+        var _currentTime = (new Date()).getTime()
+        var _timeElapsed = (_currentTime - timeStamp) / 1000
+        var player = players[socket.id] || {}
         var nextPosition = {
             x: _timeElapsed * speed.x,
             y: _timeElapsed * speed.y
         }
-        player.x = position.x + nextPosition.x;
-        player.y = position.y + nextPosition.y;
+        player.x = position.x + nextPosition.x
+        player.y = position.y + nextPosition.y
 
-    });
+    })
 
-});
-
-
-setInterval(function () {
-    io.sockets.emit('state', players);
-}, 1000 / 60);
+})
 
 
 setInterval(function () {
-    console.log("server active");
-}, 10000);
+    io.sockets.emit('state', players)
+}, 1000 / 60)
+
+
+setInterval(function () {
+    console.log("server active")
+}, 10000)
